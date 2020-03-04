85ec06c7d6847bb90fa723f4774d51081f799d67
Virgile Mendes <mendes.virgile@gmail.com>
2020-03-04 22:02:57 +0100

    commit3
    HTML
    - changement de <button> vers <input>
    JS
    - Optimisation des déclarations de variables et paramètres
    - Création de la fonction calculSimulation qui retourne le total net
    - Interaction jQuery qui affiche dans la console calculSimulation en cliquant sur le bouton "Calculer !"

--- assets/app.js
+++ assets/app.js
@@ -2,52 +2,60 @@
 
 $(document).ready(function() {
   
-  /* déclaration des rendements euros de 2000 à 2018 */
-  var eurosTab = [5.3,5.3,4.8,4.5,4.4,4.2,4.1,4.1,4.0,3.6,3.4,3.0,2.9,2.8,2.5,2.3,1.9,1.8,1.8];
+  // déclarations des rendements euros et des performances moyennes des unités de compte de 2000 à 2018
+  var eurosTab = [5.3,5.3,4.8,4.5,4.4,4.2,4.1,4.1,4.0,3.6,3.4,3.0,2.9,2.8,2.5,2.3,1.9,1.8,1.8], ucTab = [-2.0,-9.5,-15.2,8.4,6.4,14.4,8.8,1.5,-22.3,14.4,5.2,-7,11.0,8.2,5.9,4.1,3.9,5.3,-8.9];
+
+  var sommeMois, sommeAnniversaire, partEuros, partUc, fraisSouscription,fraisGestion,impot,i;
+
+  function calculSimulation () {
+
+  // déclarations des valeurs /mois et /anniversaire DEFAULT
+    //sommeMois = $("#sommeMois").val()
+  sommeMois = 10, sommeAnniversaire = 100;
   
-  /* déclaration des performances moyennes des unités de compte de 2000 à 2018 */
-  var UcTab = [-2.0,-9.5,-15.2,8.4,6.4,14.4,8.8,1.5,-22.3,14.4,5.2,-7,11.0,8.2,5.9,4.1,3.9,5.3,-8,9];
+  // déclarations des parts fonds euros et unités de compte DEFAULT
+  partEuros = 81.93, partUc = 18.07;
 
-  /* Déclaration des valeurs /mois et /anniversaire */
-  var sommeMois = 10;
-  var sommeAnniversaire = 100;
-  
-  /* Déclaration des parts fonds euros et unités de compte ¨*/
-  var partEuros = 81.93;
-  var partUc = 18.07;
+  // déclaration des différents frais prélevés DEFAULT
+  fraisSouscription = 2.5, fraisGestion = 0.7, impot = 17.2;
 
-  /* Déclaration des différents frais prélevés */
-  var fraisSouscription = 2.5;
-  var fraisGestion = 0.7;
-  var impot = 17.2;
+  // calculs de la simulation : Etapes 2000 à 2018 BRUT
 
-  /* Calculs de la simulation : Etape 2000 COURT */
-  var calcul = (sommeAnniversaire+sommeMois*12)*(1-(fraisSouscription/100));
-  console.log(calcul);
+  i = 0;
 
-  calcul = ((calcul*(partEuros/100))*(1+(eurosTab[0]/100)))+((calcul*(partUc/100))*(1+(UcTab[0]/100)));
-  console.log(calcul);
-
-  calcul = calcul*(1-(fraisGestion/100));
-  console.log(calcul);
-
-  /* Calculs de la simulation : Etapes 2001 à 2018 BROUILLON*/
-  var i = 1;
   while (i<=18) {
-  calcul = calcul+((sommeAnniversaire+sommeMois*12)*(1-(fraisSouscription/100)));
 
-  calcul = ((calcul*(partEuros/100))*(1+(eurosTab[i]/100)))+((calcul*(partUc/100))*(1+(UcTab[i]/100)));
+    if (i>0) {
+      calcul = calcul+((sommeAnniversaire+sommeMois*12)*(1-(fraisSouscription/100)));
+    } else {
+      var calcul = (sommeAnniversaire+sommeMois*12)*(1-(fraisSouscription/100));
+    }
 
-  calcul = calcul*(1-(fraisGestion/100));
+    calcul = ((calcul*(partEuros/100))*(1+(eurosTab[i]/100)))+((calcul*(partUc/100))*(1+(ucTab[i]/100)));
 
-  console.log(calcul);
+    calcul = calcul*(1-(fraisGestion/100));
 
-  i++;
+    //console.log(calcul);
+
+    i++;
 
   }
 
-  console.log('total brut : '+calcul);
+    //console.log('total brut : '+calcul);
 
+  // calcul total NET
+  calcul = ((sommeAnniversaire+sommeMois*12)*18)+(calcul-((sommeAnniversaire+sommeMois*12)*18))*(1-(impot/100));
 
+    //console.log('total net : '+calcul);
+
+  return calcul
+  }
+
+  calculSimulation ();
+
+  $("#startCalcul").on("click",function(){
+    var resultat = calculSimulation ();
+    console.log(resultat);
   });
 
+  });
